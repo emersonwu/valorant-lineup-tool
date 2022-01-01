8ac175a334196d91c4df934e38a1b79bbbb99218
Emerson <emersonwu29@gmail.com>
2022-01-01 11:29:52 +0800

    Adds LatLongMarker to DevTools a moveable marker that displays its latlng

---
+++ src/components/DeveloperTools/LatLongMarker.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <l-marker :icon="marker.icon" :lat-lng.sync="marker.position" draggable />
+    <l-control class="lat-lng-display" position="bottomleft">
+      {{ "lat: " + marker.position.lat.toString() }}
+      <br />
+      {{ "lng: " + marker.position.lng.toString() }}
+    </l-control>
+  </div>
+</template>
+
+<script lang="ts">
+import Icons from "../../leaflet_objects/Icons";
+
+import { LControl, LMarker } from "vue2-leaflet";
+
+export default {
+  name: "LatLongMarker",
+  components: {
+    LControl,
+    LMarker,
+  },
+  data() {
+    return {
+      marker: {
+        icon: Icons.noAblility,
+        position: { lng: 0, lat: 0 },
+      },
+    };
+  },
+};
+</script>
+
+<style>
+.lat-lng-display {
+  background: #fff;
+  padding: 0 0.5em;
+  border: 1px solid #aaa;
+  border-radius: 0.1em;
+  font-size: 45px;
+}
+</style>
