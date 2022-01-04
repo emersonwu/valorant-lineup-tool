b4fe1a81ac09bad27bc8b2e8770680dc8f907117
Emerson <emersonwu29@gmail.com>
2022-01-05 04:48:50 +0800

    Finishes Bind lineups Data B site

--- src/utils/MapDataManager.ts
+++ src/utils/MapDataManager.ts
@@ -3,12 +3,13 @@ import MapData from '../interfaces/MapData'
 import MapType from '@/enums/MapType'
 
 // Map imports
-import bindMapData from '../data/bind.json'
+import bindAMapData from '../data/bindA.json';
+import bindBMapData from '../data/bindB.json'
 import Coordinate from '@/interfaces/Coordinate';
 
 export default class MapDataManager {
     mapType: MapType;
-    mapJson: MapData;
+    mapJsons: MapData[];
     locations: Map<string, LocationInfo> = new Map();
     coordinates: Map<string, Coordinate> = new Map();
     spikeLineups: Map<string, String[]> = new Map();
@@ -16,47 +17,56 @@ export default class MapDataManager {
 
     constructor(mapType: MapType) {
         this.mapType = mapType;
-        this.mapJson = this.getMapDataFromJson(mapType);
+        this.mapJsons = this.getMapDataFromJson(mapType);
         this.setLocations();
         this.setCoordinates();
         this.setSpikeLineups();
     }
 
-    private getMapDataFromJson(mapType: MapType): MapData {
+    private getMapDataFromJson(mapType: MapType): MapData[] {
         switch (mapType as MapType) {
             case MapType.BIND:
-                console.log("json retrieved", bindMapData);
-                return bindMapData;
+                return [bindAMapData, bindBMapData];
             default:
                 console.log(`Maptype of ${mapType} is not a valid or configured map type.`);
                 // To prevent errors for now
-                return bindMapData;
+                return [bindAMapData, bindBMapData];
         }
     }
 
     public setLocations(): void {
-        this.mapJson.locations.forEach((locationInfo) => {
-            this.locations.set(locationInfo.key, locationInfo)
+        this.mapJsons.forEach((mapJson) => {
+            mapJson.locations.forEach((locationInfo) => {
+                this.locations.set(locationInfo.key, locationInfo)
+            })
         })
     }
 
     public setCoordinates(): void {
-        this.mapJson.coordinates.forEach((coordinate) => {
-            this.coordinates.set(coordinate.key, coordinate)
+        this.mapJsons.forEach((mapJson) => {
+            mapJson.coordinates.forEach((coordinate) => {
+                this.coordinates.set(coordinate.key, coordinate)
+            })
         })
     }
 
     public setSpikeLineups(): void {
-        this.mapJson.spikeLineups.forEach((spikeLineup) => {
-            this.spikeLineups.set(spikeLineup.spikeKey, spikeLineup.lineupKeys)
+        this.mapJsons.forEach((mapJson) => {
+            mapJson.spikeLineups.forEach((spikeLineup) => {
+                this.spikeLineups.set(spikeLineup.spikeKey, spikeLineup.lineupKeys)
+            })
         })
     }
 
     public getViperMollys(): string[] {
-        return this.mapJson.viperMollys;
+        const viperMolly: string[] = [];
+        this.mapJsons.forEach((mapJson) => viperMolly.push(...mapJson.viperMollys));
+        return viperMolly;
     }
 
     public getStandardMollys(): string[] {
-        return this.mapJson.standardMollys;
+        const standardMolly: string[] = [];
+        this.mapJsons.forEach((mapJson) => standardMolly.push(...mapJson.standardMollys));
+        return standardMolly;
     }
 }
